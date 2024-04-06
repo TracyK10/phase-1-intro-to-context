447bffba625e6aaf63630a90f1cf39587637c0ff
Tracy Karanja <chirutracy@gmail.com>
2024-04-06 23:10:13 +0300

    first commit

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 // Your code here
-function createEmployeeRecord(first_name, family_name, title, pay_per_hour){
+function createEmployeeRecord(firstName, familyName, title, payPerHour){
     // creating a new record
     let record = {
-        firstName: first_name,
-        familyName: family_name,
-        title: title,
-        timeInEvents:[],
+        firstName,
+        familyName,
+        title,
+        payPerHour,
+        timeInEvents: [],
         timeOutEvents: []
     }
     return record
@@ -13,32 +14,32 @@ function createEmployeeRecord(first_name, family_name, title, pay_per_hour){
 
 function createEmployeeRecords(array){
     // Use the map function to apply the createEmployeeRecord function to each element in the array
-    array.map(record => createEmployeeRecord(record))
-    return array
+    let record =[]
+    array.map(record => record.push(createEmployeeRecord.apply(null, record)))
 }
 
-function createTimeInEvent(recordObj, date){
+function createTimeInEvent(dateStamp){
     // Split the date string into a date and hour string
     let [date, hour] = dateStamp.split(' ');
      // Add a new time in event to the employee record with the given date and hour
-    recordObj.timeInEvents.push({
+    this.timeInEvents.push({
         type: "TimeIn",
         hour: parseInt(hour),
         date: date
     });
-    return recordObj;
+    return this;
 }
 
-function createTimeOutEvent(recordObj, date){
+function createTimeOutEvent(dateStamp){
     // Split the date string into a date and hour string
     let [date, hour] = dateStamp.split(' ');
      // Add a new time in event to the employee record with the given date and hour
-    recordObj.timeOutEvents.push({
-        type: "TimeIn",
+    this.timeOutEvents.push({
+        type: "TimeOut",
         hour: parseInt(hour),
         date: date
     });
-    return recordObj;
+    return this;
 }
 
 function hoursWorkedOnDate(obj, date){
@@ -51,12 +52,13 @@ function hoursWorkedOnDate(obj, date){
         // Calculate the hours worked by subtracting the time out hour from the time in hour and dividing by 100
         return (timeOutEvent.hour - timeInEvent.hour) / 100;
     }
+    return 0
 }
 
 function wagesEarnedOnDate(record, date){
     // Calculate the hours worked on the given date
-    let hoursWorked = hoursWorkedOnDate(record, date);
-    return hoursWorked * record.payPerHour;
+    let hoursWorked = hoursWorkedOnDate.call(this, date);
+    return hoursWorked * this.payPerHour;
 }
 
 function allWagesFor(obj){
@@ -71,8 +73,13 @@ function allWagesFor(obj){
 function calculatePayroll(array){
     // accumulate the value of all dates worked by the employee in the record used as context. Amount should be returned as a number.
     let totalPayroll = 0;
-    records.forEach(record => {
-        totalPayroll += allWagesFor(record);
+    array.forEach(record => {
+        totalPayroll += allWagesFor.call(record);
     });
     return totalPayroll;
+}
+
+// finding employees by first names
+function findEmployeeByFirstName(array, firstName){
+    array.find(record => record.firstName === firstName);
 }
